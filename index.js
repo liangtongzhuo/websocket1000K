0c57cb85a23c8bd11e50c4c3d09c813e2d638d6a
hi363138911 <liangya363138911@126.com>
2017-09-17 11:56:46 +0800

    update

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
 'use strict'
 
-const Koa = require('koa');
 const ws = require('ws');
-const app = new Koa();
-
-app.use(async (ctx, next) => {
-  await next();
-  ctx.response.type = 'text/html';
-  ctx.response.body = '<h1>Hello, koa2!</h1>';
-})
-
-const server = app.listen(8080, () => {
+const wss = new ws.Server({ port: 8080 }, () => {
   console.log('服务器启动');
 });
-const wss = new ws.Server({ server });
 
 wss.on('headers', (params, req) => {
   // console.log('headers:', params);
@@ -73,7 +63,7 @@ wss.on('connection', (ws, req) => {
 // 广播
 function broadcast(message) {
   wss.clients.forEach((ws) => {
-    if (ws.readyState === WebSocket.OPEN) {
+    if (ws.readyState === ws.OPEN) {
       if (message) {
         ws.send('用户:' + message);
       } else {
